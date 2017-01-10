1b43624277e649702a1d6a638e07173e8e23983c
luck <893855882@qq.com>
2017-01-10 18:11:35 +0800

    新增图片每行显示个数的设置

--- app/src/main/java/com/luck/pictureselector/MainActivity.java
+++ app/src/main/java/com/luck/pictureselector/MainActivity.java
@@ -148,6 +148,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                     options.setType(selectType);
                     options.setCopyMode(copyMode);
                     options.setCompress(isCompress);
+                    options.setImageSpanCount(4);
                     options.setMaxSelectNum(maxSelectNum - images.size());
                     options.setSelectMode(selectMode);
                     options.setShowCamera(isShow);
@@ -156,7 +157,7 @@ public class MainActivity extends AppCompatActivity implements RadioGroup.OnChec
                     options.setPreviewVideo(isPreviewVideo);
                     options.setCropW(cropW);
                     options.setCropH(cropH);
-                    options.setImageSpanCount(4);
+
                     if (theme) {
                         options.setThemeStyle(ContextCompat.getColor(MainActivity.this, R.color.blue));
                     }
