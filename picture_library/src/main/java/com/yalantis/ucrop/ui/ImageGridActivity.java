574c2f63b45a3d25aaeaccf23ef5102cffbcb0f0
luck <893855882@qq.com>
2017-01-17 19:04:55 +0800

    优化代码

--- picture_library/src/main/java/com/yalantis/ucrop/ui/ImageGridActivity.java
+++ picture_library/src/main/java/com/yalantis/ucrop/ui/ImageGridActivity.java
@@ -5,6 +5,8 @@ import android.content.BroadcastReceiver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.ApplicationInfo;
+import android.content.pm.PackageManager;
 import android.database.Cursor;
 import android.media.MediaMetadataRetriever;
 import android.net.Uri;
@@ -17,6 +19,7 @@ import android.support.v4.content.FileProvider;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SimpleItemAnimator;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.view.View;
 import android.view.animation.Animation;
@@ -407,7 +410,7 @@ public class ImageGridActivity extends BaseActivity implements PublicTitleBar.On
             cameraPath = cameraFile.getAbsolutePath();
             Uri imageUri;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                imageUri = FileProvider.getUriForFile(mContext, "com.jph.takephoto.fileprovider", cameraFile);//通过FileProvider创建一个content类型的Uri
+                imageUri = FileProvider.getUriForFile(mContext, getPackageName(), cameraFile);//通过FileProvider创建一个content类型的Uri
             } else {
                 imageUri = Uri.fromFile(cameraFile);
             }
@@ -426,7 +429,7 @@ public class ImageGridActivity extends BaseActivity implements PublicTitleBar.On
             cameraPath = cameraFile.getAbsolutePath();
             Uri imageUri;
             if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-                imageUri = FileProvider.getUriForFile(mContext, "com.jph.takephoto.fileprovider", cameraFile);//通过FileProvider创建一个content类型的Uri
+                imageUri = FileProvider.getUriForFile(mContext, getPackageName(), cameraFile);//通过FileProvider创建一个content类型的Uri
             } else {
                 imageUri = Uri.fromFile(cameraFile);
             }
@@ -437,30 +440,6 @@ public class ImageGridActivity extends BaseActivity implements PublicTitleBar.On
         }
     }
 
-    public static Uri getImageContentUri(Context context, File imageFile) {
-        String filePath = imageFile.getAbsolutePath();
-        Cursor cursor = context.getContentResolver().query(
-                MediaStore.Images.Media.EXTERNAL_CONTENT_URI,
-                new String[]{MediaStore.Images.Media._ID},
-                MediaStore.Images.Media.DATA + "=? ",
-                new String[]{filePath}, null);
-
-        if (cursor != null && cursor.moveToFirst()) {
-            int id = cursor.getInt(cursor
-                    .getColumnIndex(MediaStore.MediaColumns._ID));
-            Uri baseUri = Uri.parse("content://media/external/images/media");
-            return Uri.withAppendedPath(baseUri, "" + id);
-        } else {
-            if (imageFile.exists()) {
-                ContentValues values = new ContentValues();
-                values.put(MediaStore.Images.Media.DATA, filePath);
-                return context.getContentResolver().insert(
-                        MediaStore.Images.Media.EXTERNAL_CONTENT_URI, values);
-            } else {
-                return null;
-            }
-        }
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
