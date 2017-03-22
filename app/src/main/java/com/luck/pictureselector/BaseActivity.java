f847f088a699f6cf187120377d6d78e5821fa796
luck <893855882@qq.com>
2017-03-22 16:27:20 +0800

    新增单独拍照功能

--- app/src/main/java/com/luck/pictureselector/BaseActivity.java
+++ app/src/main/java/com/luck/pictureselector/BaseActivity.java
@@ -15,7 +15,7 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_base);
+
 
     }
 
