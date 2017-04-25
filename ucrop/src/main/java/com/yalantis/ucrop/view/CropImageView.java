81ea5263bd330238c337a8807e24fdf089abf0f3
luck <893855882@qq.com>
2017-04-25 15:36:38 +0800

    修改参数构建方式，采用builder模式 优化部分代码 优化图片加载方式

--- ucrop/src/main/java/com/yalantis/ucrop/view/CropImageView.java
+++ ucrop/src/main/java/com/yalantis/ucrop/view/CropImageView.java
@@ -324,7 +324,7 @@ public class CropImageView extends TransformImageView {
      * First, un-rotate image and crop rectangles (make image rectangle axis-aligned).
      * Second, calculate deltas between those rectangles sides.
      * Third, depending on delta (its sign) put them or zero inside an array.
-     * Fourth, using Matrix, rotate back those points (indents).
+     * Fourth, using Matrix, rotate picture_back those points (indents).
      *
      * @return - the float array of image indents (4 floats) - in this order [left, top, right, bottom]
      */
