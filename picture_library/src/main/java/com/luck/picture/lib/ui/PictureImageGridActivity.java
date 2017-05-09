7eabcb5de3d3f5f8725a668c90f03519a21e7df0
luck <893855882@qq.com>
2017-05-09 18:22:11 +0800

    修复中兴手机单独拍照闪退问题

--- picture_library/src/main/java/com/luck/picture/lib/ui/PictureImageGridActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/ui/PictureImageGridActivity.java
@@ -5,7 +5,6 @@ import android.content.Intent;
 import android.content.res.Configuration;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
-import android.graphics.Color;
 import android.media.MediaMetadataRetriever;
 import android.media.SoundPool;
 import android.net.Uri;
@@ -128,10 +127,8 @@ public class PictureImageGridActivity extends PictureBaseActivity implements Vie
             }
             if (!enableCrop && isCompress) {
                 // 如果单独拍照，并且没有裁剪 但压缩 这里显示一个蒙版过渡一下
+                setContentView(R.layout.picture_empty);
                 ToolbarUtil.setColorNoTranslucent(this, R.color.black);
-                TextView view = new TextView(this);
-                view.setBackgroundColor(Color.BLACK);
-                setContentView(view);
             }
         } else {
             setContentView(R.layout.picture_activity_image_grid);
