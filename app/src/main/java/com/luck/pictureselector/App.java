a8071b0e8c7776f642826a2bec0528abf9f2fc15
luck <893855882@qq.com>
2017-05-11 12:49:59 +0800

    commit

---
+++ app/src/main/java/com/luck/pictureselector/App.java
@@ -0,0 +1,29 @@
+package com.luck.pictureselector;
+
+import android.app.Application;
+
+import com.squareup.leakcanary.LeakCanary;
+import com.tencent.bugly.crashreport.CrashReport;
+
+/**
+ * author：luck
+ * project：PictureSelector
+ * package：com.luck.pictureselector
+ * email：893855882@qq.com
+ * data：2017/4/29
+ */
+
+public class App extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        LeakCanary.install(this);
+        //bugly追踪的上报测试时候设置为true
+        CrashReport.initCrashReport(getApplicationContext(), "84c42856dd", true);
+    }
+}
