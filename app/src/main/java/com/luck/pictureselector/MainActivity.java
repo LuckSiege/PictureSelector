c1479c8e6222f970ac08ca6b937387a4f200ac15
luck <893855882@qq.com>
2017-06-08 09:43:15 +0800

    PictureSelector 2.0 第一次提交

--- app/src/main/java/com/luck/pictureselector/MainActivity.java
+++ app/src/main/java/com/luck/pictureselector/MainActivity.java
@@ -240,6 +240,11 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 case PictureConfig.CHOOSE_REQUEST:
                     // 图片选择结果回调
                     selectList = PictureSelector.obtainMultipleResult(data);
+                    // 例如 LocalMedia 里面返回三种path
+                    // 1.media.getPath(); 为原图path
+                    // 2.media.getCutPath();为裁剪后path，需判断media.isCut();是否为true
+                    // 3.media.getCompressPath();为压缩后path，需判断media.isCompressed();是否为true
+                    // 如果裁剪并压缩了，已取压缩路径为准，因为是先裁剪后压缩的
                     adapter.setList(selectList);
                     adapter.notifyDataSetChanged();
                     DebugUtil.i(TAG, "onActivityResult:" + selectList.size());
