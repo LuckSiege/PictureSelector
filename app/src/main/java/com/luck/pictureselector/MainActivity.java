d354acace3e94f5a2eda995a09562cce4af498f6
罗孟伟 <luomengwei@laoyuegou.com>
2018-02-01 21:33:08 +0800

    commit

--- app/src/main/java/com/luck/pictureselector/MainActivity.java
+++ app/src/main/java/com/luck/pictureselector/MainActivity.java
@@ -177,20 +177,21 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                         .withAspectRatio(aspect_ratio_x, aspect_ratio_y)// 裁剪比例 如16:9 3:2 3:4 1:1 可自定义
                         .hideBottomControls(cb_hide.isChecked() ? false : true)// 是否显示uCrop工具栏，默认不显示
                         .isGif(cb_isGif.isChecked())// 是否显示gif图片
-                        .freeStyleCropEnabled(cb_styleCrop.isChecked())// 裁剪框是否可拖拽
+                        .freeStyleCropEnabled(true)// 裁剪框是否可拖拽
                         .circleDimmedLayer(cb_crop_circular.isChecked())// 是否圆形裁剪
                         .showCropFrame(cb_showCropFrame.isChecked())// 是否显示裁剪矩形边框 圆形裁剪时建议设为false
                         .showCropGrid(cb_showCropGrid.isChecked())// 是否显示裁剪矩形网格 圆形裁剪时建议设为false
                         .openClickSound(cb_voice.isChecked())// 是否开启点击声音
                         .selectionMedia(selectList)// 是否传入已选图片
+                        //.isDragFrame(false)// 是否可拖动裁剪框(固定)
 //                        .videoMaxSecond(15)
 //                        .videoMinSecond(10)
                         //.previewEggs(false)// 预览图片时 是否增强左右滑动图片体验(图片滑动一半即可看到上一张是否选中)
                         //.cropCompressQuality(90)// 裁剪压缩质量 默认100
                         .minimumCompressSize(100)// 小于100kb的图片不压缩
                         //.cropWH()// 裁剪宽高比，设置如果大于图片本身宽高则无效
-                        //.rotateEnabled() // 裁剪是否可旋转图片
-                        //.scaleEnabled()// 裁剪是否可放大缩小图片
+                        //.rotateEnabled(true) // 裁剪是否可旋转图片
+                        //.scaleEnabled(true)// 裁剪是否可放大缩小图片
                         //.videoQuality()// 视频录制质量 0 or 1
                         //.videoSecond()//显示多少秒以内的视频or音频也可适用
                         //.recordVideoSecond()//录制视频秒数 默认60s
