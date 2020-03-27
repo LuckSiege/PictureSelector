bae45995f9ef6f2b719b41d90931e6545e2a1be2
罗孟伟 <luomengwei@laoyuegou.com>
2020-03-27 09:51:51 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/model/LocalMediaLoader.java
+++ picture_library/src/main/java/com/luck/picture/lib/model/LocalMediaLoader.java
@@ -166,7 +166,10 @@ public class LocalMediaLoader {
                         // 这里解决部分机型获取mimeType返回 image/* 格式导致无法判别其具体类型 例如小米8，9，10等机型
                         if (mimeType.endsWith("image/*")) {
                             if (PictureMimeType.isContent(url)) {
-                                mimeType = PictureMimeType.getImageMimeType(PictureFileUtils.getPath(mContext, Uri.parse(url)));
+                                String absolutePath = PictureFileUtils.getPath(mContext, Uri.parse(url));
+                                mimeType = PictureMimeType.getImageMimeType(absolutePath);
+                            } else {
+                                mimeType = PictureMimeType.getImageMimeType(url);
                             }
                             if (!config.isGif) {
                                 boolean isGif = PictureMimeType.isGif(mimeType);
