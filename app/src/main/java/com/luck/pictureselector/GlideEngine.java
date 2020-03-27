ea2c5bc3567af056f096aec48fe5d80eb1468be9
罗孟伟 <luomengwei@laoyuegou.com>
2020-03-27 09:44:10 +0800

    commit

--- app/src/main/java/com/luck/pictureselector/GlideEngine.java
+++ app/src/main/java/com/luck/pictureselector/GlideEngine.java
@@ -52,7 +52,7 @@ public class GlideEngine implements ImageEngine {
      * @param url
      * @param imageView
      * @param longImageView
-     * @param callback      网络图片加载回调监听
+     * @param callback      网络图片加载回调监听 {link version 2.5.1 after Please use the #OnImageCompleteCallback#}
      */
     @Override
     public void loadImage(@NonNull Context context, @NonNull String url,
