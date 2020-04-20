e252b6e546bdcfe7c8fab0929e104ed0db62ebf0
罗孟伟 <luomengwei@laoyuegou.com>
2020-04-20 17:29:06 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/PictureSelectorActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/PictureSelectorActivity.java
@@ -1164,12 +1164,14 @@ public class PictureSelectorActivity extends PictureBaseActivity implements View
                 mTvPictureTitle.setTag(R.id.view_count_tag, folderWindow.getFolder(position) != null
                         ? folderWindow.getFolder(position).getImageNum() : 0);
                 mPage = 1;
-                mAdapter.clear();
                 showPleaseDialog();
                 LocalMediaPageLoader.getInstance(getContext(), config).loadPageMediaData(bucketId, mPage,
                         (OnQueryDataResultListener<LocalMedia>) (result, isHasMore) -> {
                             this.isHasMore = isHasMore;
                             if (!isFinishing()) {
+                                if (result.size() == 0) {
+                                    mAdapter.clear();
+                                }
                                 mAdapter.bindData(result);
                                 mRecyclerView.onScrolled(0, 0);
                                 mRecyclerView.smoothScrollToPosition(0);
