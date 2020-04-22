7df388f628f3571063ddb37aaefe68024c5ad1c1
罗孟伟 <luomengwei@laoyuegou.com>
2020-04-22 10:58:19 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/PictureBaseActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/PictureBaseActivity.java
@@ -3,7 +3,6 @@ package com.luck.picture.lib;
 import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.ActivityInfo;
 import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Build;
@@ -154,7 +153,6 @@ public abstract class PictureBaseActivity extends AppCompatActivity {
         // 单独拍照不设置主题因为拍照界面已经设置了透明主题了
         if (!config.camera) {
             setTheme(config.themeStyleId == 0 ? R.style.picture_default_style : config.themeStyleId);
-            config.requestedOrientation = config.themeStyleId == 0 ? ActivityInfo.SCREEN_ORIENTATION_UNSPECIFIED : config.requestedOrientation;
         }
         super.onCreate(savedInstanceState == null ? new Bundle() : savedInstanceState);
         if (isRequestedOrientation()) {
@@ -181,7 +179,7 @@ public abstract class PictureBaseActivity extends AppCompatActivity {
 
     private void isCheckConfigNull() {
         if (config == null) {
-            config = PictureSelectionConfig.getInstance();
+            config = PictureSelectionConfig.getCleanInstance();
         }
     }
 
@@ -277,10 +275,8 @@ public abstract class PictureBaseActivity extends AppCompatActivity {
 
     @Override
     protected void attachBaseContext(Context newBase) {
-        config = PictureSelectionConfig.getInstance();
-        if (config != null) {
-            super.attachBaseContext(PictureContextWrapper.wrap(newBase, config.language));
-        }
+        config = PictureSelectionConfig.getCleanInstance();
+        super.attachBaseContext(PictureContextWrapper.wrap(newBase, config.language));
     }
 
     /**
