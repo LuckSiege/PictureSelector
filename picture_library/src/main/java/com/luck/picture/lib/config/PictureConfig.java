20ac05e9df7983ca4f64e3cc785d19aa55e33858
罗孟伟 <luomengwei@laoyuegou.com>
2020-04-24 13:29:28 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/config/PictureConfig.java
+++ picture_library/src/main/java/com/luck/picture/lib/config/PictureConfig.java
@@ -39,7 +39,7 @@ public final class PictureConfig {
 
     public final static String EXTRA_ALL_FOLDER_SIZE = "all_folder_size";
 
-    public final static int MAX_PAGE_SIZE = 20;
+    public final static int MAX_PAGE_SIZE = 60;
 
     public final static int MIN_PAGE_SIZE = 10;
 
