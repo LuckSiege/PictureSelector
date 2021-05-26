a3f46886300c6b97d65bb68b375bc981dd323cdf
罗孟伟 <luomengwei@021.com>
2021-05-26 13:42:08 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/PictureBaseActivity.java
+++ picture_library/src/main/java/com/luck/picture/lib/PictureBaseActivity.java
@@ -10,7 +10,6 @@ import android.os.Handler;
 import android.os.Looper;
 import android.provider.MediaStore;
 import android.text.TextUtils;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
