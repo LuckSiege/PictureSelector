444e649ee37be4f8f02414dff9ce00f5854bd04b
罗孟伟 <luomengwei@021.com>
2021-08-01 21:02:24 +0800

    commit

--- picture_library/src/main/java/com/luck/picture/lib/tools/PictureFileUtils.java
+++ picture_library/src/main/java/com/luck/picture/lib/tools/PictureFileUtils.java
@@ -27,7 +27,6 @@ import java.io.Closeable;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.lang.annotation.Retention;
