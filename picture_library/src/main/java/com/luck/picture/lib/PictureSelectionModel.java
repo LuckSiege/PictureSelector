43c0642225c3ee91ae04899071bed4cb1494bcaa
JG Pak <leaf9016@gmail.com>
2021-12-16 13:43:23 +0900

    삭제된 코드 추가

--- picture_library/src/main/java/com/luck/picture/lib/PictureSelectionModel.java
+++ picture_library/src/main/java/com/luck/picture/lib/PictureSelectionModel.java
@@ -1742,6 +1742,36 @@ public class PictureSelectionModel {
     }
 
     /**
+     * Doctalk 사용
+     * @param intent
+     * @param listener
+     */
+    public void forResult (Intent intent, OnResultCallbackListener<LocalMedia> listener) {
+        if (!DoubleUtils.isFastDoubleClick()) {
+            Activity activity = selector.getActivity();
+            if (activity == null || selectionConfig == null) {
+                return;
+            }
+            if (PictureSelectionConfig.imageEngine == null){
+                throw new NullPointerException("api imageEngine is null,Please implement ImageEngine");
+            }
+            // 绑定回调监听
+            PictureSelectionConfig.listener = new WeakReference<>(listener).get();
+            selectionConfig.isCallbackMode = true;
+            Fragment fragment = selector.getFragment();
+            if (fragment != null) {
+                fragment.startActivity(intent);
+            }
+            else {
+                activity.startActivity(intent);
+            }
+            PictureWindowAnimationStyle windowAnimationStyle = PictureSelectionConfig.windowAnimationStyle;
+            activity.overridePendingTransition(
+                    windowAnimationStyle.activityEnterAnimation, R.anim.picture_anim_fade_in);
+        }
+    }
+
+    /**
      * Start to select media and wait for result.
      *
      * @param launcher
@@ -1763,6 +1793,27 @@ public class PictureSelectionModel {
                         ? PictureSelectorCameraEmptyActivity.class :
                         selectionConfig.isWeChatStyle ? PictureSelectorWeChatStyleActivity.class
                                 : PictureSelectorActivity.class);
+            }
+            selectionConfig.isCallbackMode = false;
+            launcher.launch(intent);
+            PictureWindowAnimationStyle windowAnimationStyle = PictureSelectionConfig.windowAnimationStyle;
+            activity.overridePendingTransition(windowAnimationStyle.activityEnterAnimation, R.anim.picture_anim_fade_in);
+        }
+    }
+
+    /**
+     * Start to select media and wait for result.
+     *
+     * @param launcher
+     */
+    public void forResult(Intent intent, ActivityResultLauncher<Intent> launcher) {
+        if (!DoubleUtils.isFastDoubleClick()) {
+            Activity activity = selector.getActivity();
+            if (launcher == null || activity == null || selectionConfig == null) {
+                return;
+            }
+            if (PictureSelectionConfig.imageEngine == null){
+                throw new NullPointerException("api imageEngine is null,Please implement ImageEngine");
             }
             selectionConfig.isCallbackMode = false;
             launcher.launch(intent);
