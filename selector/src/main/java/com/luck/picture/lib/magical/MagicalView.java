37c18afd66756ba7c3f15c4ace5ed1d54d29226c
罗孟伟 <luomengwei@021.com>
2021-12-17 23:42:22 +0800

    commit

--- selector/src/main/java/com/luck/picture/lib/magical/MagicalView.java
+++ selector/src/main/java/com/luck/picture/lib/magical/MagicalView.java
@@ -31,7 +31,7 @@ import com.luck.picture.lib.utils.DensityUtil;
 public class MagicalView extends FrameLayout {
 
     private float mAlpha = 0.0F;
-    private final long animationDuration = 250;
+    private final long animationDuration = 2500;
     private int mOriginLeft;
     private int mOriginTop;
     private int mOriginHeight;
