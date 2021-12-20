2b956824db4a8e982a8d1dfe25d8b1fac1121df3
罗孟伟 <luomengwei@021.com>
2021-12-20 13:05:19 +0800

    commit

--- selector/src/main/java/com/luck/picture/lib/adapter/holder/BasePreviewHolder.java
+++ selector/src/main/java/com/luck/picture/lib/adapter/holder/BasePreviewHolder.java
@@ -1,9 +1,11 @@
 package com.luck.picture.lib.adapter.holder;
 
 import android.graphics.Bitmap;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
 import android.widget.ImageView;
 
 import androidx.annotation.NonNull;
@@ -16,6 +18,7 @@ import com.luck.picture.lib.entity.LocalMedia;
 import com.luck.picture.lib.interfaces.OnCallbackListener;
 import com.luck.picture.lib.photoview.OnViewTapListener;
 import com.luck.picture.lib.photoview.PhotoView;
+import com.luck.picture.lib.utils.DensityUtil;
 import com.luck.picture.lib.utils.MediaUtils;
 
 /**
@@ -32,6 +35,9 @@ public class BasePreviewHolder extends RecyclerView.ViewHolder {
      * 视频
      */
     public final static int ADAPTER_TYPE_VIDEO = 2;
+    private final int screenWidth;
+    private final int screenHeight;
+    private PictureSelectionConfig config;
     public PhotoView coverImageView;
 
     public static BasePreviewHolder generate(ViewGroup parent, int viewType, int resource) {
@@ -45,6 +51,9 @@ public class BasePreviewHolder extends RecyclerView.ViewHolder {
 
     public BasePreviewHolder(@NonNull View itemView) {
         super(itemView);
+        this.config = PictureSelectionConfig.getInstance();
+        this.screenWidth = DensityUtil.getScreenWidth(itemView.getContext());
+        this.screenHeight = DensityUtil.getScreenHeight(itemView.getContext());
         this.coverImageView = itemView.findViewById(R.id.preview_image);
     }
 
@@ -73,6 +82,22 @@ public class BasePreviewHolder extends RecyclerView.ViewHolder {
                 mPreviewEventListener.onLoadCompleteBeginScale(BasePreviewHolder.this);
             }
         });
+
+        if (!config.isPreviewZoomEffect && !config.isPreviewFullScreenMode) {
+            if (screenWidth < screenHeight) {
+                float width = Math.min(media.getWidth(), media.getHeight());
+                float height = Math.max(media.getHeight(), media.getWidth());
+                if (width > 0 && height > 0) {
+                    // 只需让图片的宽是屏幕的宽，高乘以比例
+                    int displayHeight = (int) Math.ceil(width * height / width);
+                    //最终让图片按照宽是屏幕 高是等比例缩放的大小
+                    FrameLayout.LayoutParams layoutParams = (FrameLayout.LayoutParams) coverImageView.getLayoutParams();
+                    layoutParams.width = screenWidth;
+                    layoutParams.height = displayHeight < screenHeight ? displayHeight + screenHeight : displayHeight;
+                    layoutParams.gravity = Gravity.CENTER;
+                }
+            }
+        }
 
         coverImageView.setOnViewTapListener(new OnViewTapListener() {
             @Override
