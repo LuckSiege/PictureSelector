797fbe36b46c487c8a7e04c1be05deefce351c2f
罗孟伟 <luomengwei@021.com>
2022-01-15 18:10:55 +0800

    commit

--- selector/src/main/java/com/luck/picture/lib/utils/ToastUtils.java
+++ selector/src/main/java/com/luck/picture/lib/utils/ToastUtils.java
@@ -1,9 +1,11 @@
 package com.luck.picture.lib.utils;
 
 import android.content.Context;
+import android.text.TextUtils;
 import android.widget.Toast;
 
 import com.luck.picture.lib.app.PictureAppMaster;
+import com.luck.picture.lib.thread.PictureThreadUtils;
 
 /**
  * @author：luck
@@ -15,13 +17,44 @@ public class ToastUtils {
      * show toast content
      *
      * @param context
-     * @param content
+     * @param text
      */
-    public static void showToast(Context context, String content) {
+    public static void showToast(Context context, String text) {
+        if (isFastDoubleClick() && TextUtils.equals(text, mLastText)) {
+            return;
+        }
         Context appContext = PictureAppMaster.getInstance().getAppContext();
         if (appContext == null) {
             appContext = context.getApplicationContext();
         }
-        Toast.makeText(appContext, content, Toast.LENGTH_SHORT).show();
+        if (PictureThreadUtils.isInUiThread()) {
+            Toast.makeText(appContext, text, Toast.LENGTH_SHORT).show();
+            mLastText = text;
+        } else {
+            PictureThreadUtils.runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    Context appContext = PictureAppMaster.getInstance().getAppContext();
+                    if (appContext == null) {
+                        appContext = context.getApplicationContext();
+                    }
+                    Toast.makeText(appContext, text, Toast.LENGTH_SHORT).show();
+                    mLastText = text;
+                }
+            });
+        }
+    }
+
+    private final static long TIME = 1000;
+    private static long lastClickTime;
+    private static String mLastText;
+
+    public static boolean isFastDoubleClick() {
+        long time = System.currentTimeMillis();
+        if (time - lastClickTime < TIME) {
+            return true;
+        }
+        lastClickTime = time;
+        return false;
     }
 }
