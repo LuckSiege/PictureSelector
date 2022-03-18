29f83d925ca89b8d31a9b2551dece8184f4fe46c
罗孟伟 <luomengwei@021.com>
2022-03-18 23:12:27 +0800

    commit

--- selector/src/main/java/com/luck/picture/lib/widget/MediumBoldTextView.java
+++ selector/src/main/java/com/luck/picture/lib/widget/MediumBoldTextView.java
@@ -7,7 +7,6 @@ import android.graphics.Paint;
 import android.text.TextPaint;
 import android.util.AttributeSet;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.widget.AppCompatTextView;
 
 import com.luck.picture.lib.R;
@@ -21,20 +20,18 @@ public class MediumBoldTextView extends AppCompatTextView {
     private float mStrokeWidth = 0.6F;
 
     public MediumBoldTextView(Context context) {
-        super(context);
-        initView(context, null);
+        this(context, null);
     }
 
-    public MediumBoldTextView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-        initView(context, attrs);
+    public MediumBoldTextView(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
     }
 
-    private void initView(Context context, @Nullable AttributeSet attrs) {
-        if (attrs != null) {
-            TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.PsMediumBoldTextView);
-            mStrokeWidth = array.getFloat(R.styleable.PsMediumBoldTextView_stroke_Width, mStrokeWidth);
-        }
+    public MediumBoldTextView(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.PictureMediumBoldTextView, defStyleAttr, 0);
+        mStrokeWidth = a.getFloat(R.styleable.PictureMediumBoldTextView_stroke_Width, mStrokeWidth);
+        a.recycle();
     }
 
     @Override
---
+++ selector/src/main/java/com/luck/picture/lib/widget/RoundCornerRelativeLayout.java
@@ -0,0 +1,71 @@
+package com.luck.picture.lib.widget;
+
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Path;
+import android.graphics.RectF;
+import android.util.AttributeSet;
+import android.widget.RelativeLayout;
+
+import com.luck.picture.lib.R;
+
+/**
+ * @author：luck
+ * @date：2022/3/18 10:39 下午
+ * @describe：RoundCornerRelativeLayout
+ */
+public class RoundCornerRelativeLayout extends RelativeLayout {
+    private final Path path;
+    private final float cornerSize;
+    private final boolean isTopNormal;
+    private final boolean isBottomNormal;
+    private final RectF mRect = new RectF();
+
+    public RoundCornerRelativeLayout(Context context) {
+        this(context, null);
+    }
+
+    public RoundCornerRelativeLayout(Context context, AttributeSet attrs) {
+        this(context, attrs, 0);
+    }
+
+    public RoundCornerRelativeLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs, R.styleable.PictureRoundCornerRelativeLayout, defStyleAttr, 0);
+        cornerSize = a.getDimension(R.styleable.PictureRoundCornerRelativeLayout_corners, 0);
+        isTopNormal = a.getBoolean(R.styleable.PictureRoundCornerRelativeLayout_topNormal, false);
+        isBottomNormal = a.getBoolean(R.styleable.PictureRoundCornerRelativeLayout_bottomNormal, false);
+        a.recycle();
+        path = new Path();
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldW, int oldH) {
+        super.onSizeChanged(w, h, oldW, oldH);
+        path.reset();
+        mRect.right = w;
+        mRect.bottom = h;
+        float[] cornerRadii;
+        if (!isTopNormal && !isBottomNormal) {
+            path.addRoundRect(mRect, cornerSize, cornerSize, Path.Direction.CW);
+        } else {
+            if (isTopNormal) {
+                cornerRadii = new float[]{0, 0, 0, 0, cornerSize, cornerSize, cornerSize, cornerSize};
+                path.addRoundRect(mRect, cornerRadii, Path.Direction.CW);
+            }
+            if (isBottomNormal) {
+                cornerRadii = new float[]{cornerSize, cornerSize, cornerSize, cornerSize, 0, 0, 0, 0};
+                path.addRoundRect(mRect, cornerRadii, Path.Direction.CW);
+            }
+        }
+    }
+
+    @Override
+    protected void dispatchDraw(Canvas canvas) {
+        canvas.save();
+        canvas.clipPath(path);
+        super.dispatchDraw(canvas);
+        canvas.restore();
+    }
+}
